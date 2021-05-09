4005f09862d0cdf4aececb6f9aa3b6446837f90c
dr497 <anand.ratiney@gmail.com>
2021-05-09 17:58:46 +0800

    temp fix with manual slicing

--- name-service/js/src/state.ts
+++ name-service/js/src/state.ts
@@ -11,46 +11,57 @@ export class NameRegistryState {
     [
       NameRegistryState,
       {
-        kind: 'struct',
+        kind: "struct",
         fields: [
-          ['parentName', [32]],
-          ['owner', [32]],
-          ['class', [32]],
-          ['data', ['u8']],
+          ["parentName", [32]],
+          ["owner", [32]],
+          ["class", [32]],
+          ["data", ["u8"]],
         ],
       },
     ],
   ]);
   constructor(obj: {
-    parentName: Uint8Array;
-    owner: Uint8Array;
-    class: Uint8Array;
-    data: Uint8Array;
+    parentName: Buffer;
+    owner: Buffer;
+    class: Buffer;
+    data: Buffer;
   }) {
     this.parentName = new PublicKey(obj.parentName);
     this.owner = new PublicKey(obj.owner);
     this.class = new PublicKey(obj.class);
-    this.data = Buffer.from(obj.data);
+    this.data = obj.data;
   }
 
   public static async retrieve(
     connection: Connection,
-    nameAccountKey: PublicKey,
+    nameAccountKey: PublicKey
   ): Promise<NameRegistryState> {
     let nameAccount = await connection.getAccountInfo(
       nameAccountKey,
-      'processed',
+      "processed"
     );
     if (!nameAccount) {
-      throw new Error('Invalid name account provided');
+      throw new Error("Invalid name account provided");
     }
 
-    let res: NameRegistryState = deserializeUnchecked(
-      this.schema,
-      NameRegistryState,
-      nameAccount.data,
-    );
-    return res;
-  }
+    // let res: NameRegistryState = deserializeUnchecked(
+    //   this.schema,
+    //   NameRegistryState,
+    //   nameAccount.data,
 
+    // );
+
+    const parentName = nameAccount.data?.slice(0, 32);
+    const owner = nameAccount.data?.slice(32, 64);
+    const className = nameAccount.data?.slice(64, 96);
+    const data = nameAccount.data?.slice(96);
+
+    return new NameRegistryState({
+      parentName,
+      owner,
+      class: className,
+      data,
+    });
+  }
 }
