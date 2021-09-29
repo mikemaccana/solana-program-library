e5a67c74dafe923367f195a6480a1d7ffe8e2ddb
DR497 <47689875+dr497@users.noreply.github.com>
2021-09-29 16:00:56 +0800

    JS: export more things

--- name-service/js/src/index.ts
+++ name-service/js/src/index.ts
@@ -2,3 +2,6 @@ export * from "./utils";
 export * from "./bindings";
 export * from "./state";
 export * from "./twitter_bindings";
+export * from "./tokens";
+export * from "./utils";
+export * from "./instructions";
