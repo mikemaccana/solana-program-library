d3a028194106828103996a6ea880953a7bccd349
dr497 <47689875+dr497@users.noreply.github.com>
2022-07-12 12:50:20 +0800

    js: add `getAllRegisteredDomains`

--- name-service/js/src/utils.ts
+++ name-service/js/src/utils.ts
@@ -201,3 +201,27 @@ export async function getAllDomains(
   });
   return accounts.map((a) => a.pubkey);
 }
+
+/**
+ * This function can be used to retrieve all the registered `.sol` domains.
+ * The account data is sliced to avoid enormous payload and only the owner is returned
+ * @param connection The Solana RPC connection object
+ * @returns
+ */
+export const getAllRegisteredDomains = async (connection: Connection) => {
+  const filters = [
+    {
+      memcmp: {
+        offset: 0,
+        bytes: ROOT_DOMAIN_ACCOUNT.toBase58(),
+      },
+    },
+  ];
+  const dataSlice = { offset: 32, length: 32 };
+
+  const accounts = await connection.getProgramAccounts(NAME_PROGRAM_ID, {
+    dataSlice,
+    filters,
+  });
+  return accounts;
+};
